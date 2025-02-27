59f892cc249c88c72c26a4ee3a332aa02215bdd4
xu xuejian <2498492824@qq.com>
2025-02-27 19:08:43 +0800

    首页垂直轮播

--- wallpaper/pages/index/index.vue
+++ wallpaper/pages/index/index.vue
@@ -14,6 +14,23 @@
 				</swiper-item>
 			</swiper>
 		</view>
+		
+		<view class="notice">
+			<view class="left">
+				<uni-icons type="sound-filled" size="20" color="#28b389"></uni-icons>
+				<text class="text">公告</text>
+			</view>
+			<view class="center">
+				<swiper  :autoplay="true" circular :interval="1500" :duration="1000" vertical>
+					<swiper-item v-for="item in 3" :key='item'>
+						公告大师傅似的手动阀手动阀手动阀手动阀撒打发撒打发撒打发asdasdsadas
+					</swiper-item>
+				</swiper>
+			</view>
+			<view class="right">
+				<uni-icons type="right" size="16" color="#333"></uni-icons>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -25,24 +42,74 @@
 .home{
 	.banner{
 		width: 750rpx;
-		padding: 30rpx;
-		border: 1px solid red;
+		padding: 30rpx 0;
 		
-		swiper{
+		swiper{	
 			width: 100%;
 			height: 340rpx;
-			border-radius: 15rpx;
+			
 			overflow: hidden;
 			
 			swiper-item{
 					width: 100%;
 					height: 100%;
+					padding: 0 30rpx;
+					
 					// 图片的宽度高度确定时，图片需要按照父元素高度宽度设置，mode模式
 				image{
 					width: 100%;
 					height: 100%;
+					border-radius: 15rpx;
 				}
 			}
+		}
+	}
+	
+	.notice{
+		width: 690rpx;
+		height: 80rpx;
+		margin: 0 auto;
+		border-radius: 80rpx;
+		background-color: #f9f9f9;
+		display: flex;
+		align-items: center;
+		
+		.left{
+			width:140rpx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			.text{
+				color:#28b389;
+				font-weight: 600;
+			}
+		}
+		.center{
+			flex: 1;
+			height: 100%;
+			line-height: 80rpx;
+			swiper{
+					height: 100%;
+				&-item{
+					// 开了flex以后，文本溢出不显示省略号了
+					// display: flex;
+					// align-items: center;
+					height: 100%;
+					color: #666;
+					font-size: 30rpx;
+					
+					overflow: hidden;
+					text-overflow: ellipsis;
+					white-space: nowrap;
+				}
+			}
+		}
+		.right{
+			width: 70rpx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			// background-color: #28b389;
 		}
 	}
 }
